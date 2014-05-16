95e42adb7e4466c6ebf0f07ec17bf4125bd49fcd
Phillip Shum <phillip.shum@lbi.com>
2014-05-16 13:37:33 +0100

    heartbeat

--- js/canvas.js
+++ js/canvas.js
@@ -13,7 +13,7 @@
         init : function () {
 
             this.canvas = document.getElementById('Canvas');
-            this.context = this.canvas.getContext("2d");
+            this.ctx = this.canvas.getContext("2d");
             this.bindEvents();
         },
         bindEvents : function () {
@@ -29,6 +29,37 @@
         },
         tickCommand : function (e) {
 
+            this.clear();
+            this.drawBeat();
+        },
+        drawBeat : function () {
+
+            this.alpha = this.alpha || 1;
+            this.direction = this.direction || 1;
+            this.delta = this.delta || 0.1;
+
+            if (this.direction === 1) {
+                this.alpha += this.delta;
+                if (this.alpha >= 1) {
+                    this.direction = -1;
+                }
+            }
+
+            if (this.direction === -1) {
+                this.alpha -= this.delta;
+                this.alpha = (this.alpha < 0.1) ? 0.1 : this.alpha;
+                if (this.alpha <= 0.2) {
+                    this.direction = 1;
+                }
+            }
+
+            this.ctx.beginPath();
+            this.ctx.arc(10, 10, 5, 0, 2 * Math.PI, false);
+            this.ctx.fillStyle = 'rgba(255, 0, 0, ' + this.alpha + ')';
+            this.ctx.fill();
+        },
+        clear : function () {
+            this.canvas.width = this.canvas.width;
         }
     }
 
